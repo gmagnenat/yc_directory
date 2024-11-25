ecf4a421ffe288256155f3792e8f0be3611bce2f
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-25 13:03:52 +0100

    feat: add a card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import { title } from "process";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -7,6 +9,21 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Gwenaël Magnenat" },
+      _id: 1,
+      description: "A platform for sharing and discovering new music",
+      image:
+        "https://images.unsplash.com/photo-1516783154360-123b392d0833?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Craft",
+      title: "Craft Workshop group class",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -18,6 +35,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p>No startup found</p>
+          )}
+        </ul>
       </section>
     </>
   );

075731568aa16ff0eef9f7f2eea9542282f401aa
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-25 12:10:19 +0100

    feat: add search form

---
+++ components/SearchFormReset.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { Search, X } from "lucide-react";
+import { Button } from "./ui/button";
+
+const SearchFormReset = () => {
+  const reset = () => {
+    const form = document.querySelector(".search-form") as HTMLFormElement;
+
+    if (form) form.reset();
+  };
+  return (
+    <Button asChild type="reset" onClick={reset}>
+      <Link href="/" className="search-btn text-white">
+        <X className="size-5" />
+      </Link>
+    </Button>
+  );
+};
+export default SearchFormReset;
